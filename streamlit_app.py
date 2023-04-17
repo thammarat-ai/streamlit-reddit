a7690bada78126db63d748a25527d6f3d2991cb2
Thammarat Thamma <thammarat.th@up.ac.th>
2023-04-17 17:56:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,26 +9,25 @@ key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
-
 # Streamlit widgets to let a user create a new post
-title = st.text_input('Post Title')
-url = st.text_input('Post URL')
-submit = st.button('Submit New Post')
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
 
-# Once the user has submitted the post, add it to Firestore
+# Once the user has submitted, upload it to the database
 if title and url and submit:
-    doc_ref = db.collection('posts').document(title)
-    doc_ref.set({
-        "title": title,
-        "url": url,
-    })
-    
-# And then render each post, using some light Markdown formatting
-posts_ref = db.collection('posts')
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
+
+# And then render each post, using some light Markdown
+posts_ref = db.collection("posts")
 for doc in posts_ref.stream():
-    post = doc.to_dict()
-    title =  post['title']
-    url = post["url"]
-    
-    st.subheader(f"Post: {title}")
-    st.write(f":link: [{url}]({url})")
+	post = doc.to_dict()
+	title = post["title"]
+	url = post["url"]
+
+	st.subheader(f"Post: {title}")
+	st.write(f":link: [{url}]({url})")
